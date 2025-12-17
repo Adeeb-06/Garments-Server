f838e44437ef94ba9a8e8ec010989ada4f1abb3d
Adeeb-06 <mohammadadeeb886@gmail.com>
2025-12-17 14:58:53 +0600

    frontend url

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173" , "https://garment-627fe.web.app"],
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
